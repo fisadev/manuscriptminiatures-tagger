582ce9faec875230d96d149e67d9394e4ca43751
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-10-07 19:02:30 -0300

    Miniature model and basic settings

---
+++ core.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import attr
+import requests
+
+import settings
+
+
+@attr.s
+class Miniature:
+    manuscript_id = attr.ib()
+    miniature_id = attr.ib()
+    file_name = attr.ib()
+    start_year = attr.ib()
+    end_year = attr.ib()
+    tags = attr.ib()
+
+    @property
+    def file_path(self):
+        return Path(settings.MINIATURES_DIR) / self.file_name
+
+    def download_file(self):
+        """
+        Download the picture file.
+        """
+        url = settings.MINIATURE_URL.format(self.file_name)
+        response = requests.get(url)
+        with self.file_path.open("wb") as picture_file:
+            picture_file.write(response.content)
+
+    def __str__(self):
+        return '<Miniature {} ({})>'.format(self.miniature_id, self.file_name)
+
+    @classmethod
+    def all(cls):
+        """
+        Read all the miniatures metadata, and return them as Miniature
+        instances (ignoring bad lines in the csv).
+        """
+        for line in open(settings.MINIATURES_METADATA).readlines():
+            fields = [field.strip()
+                      for field in line.split(',')
+                      if field.strip()]
+
+            if len(fields) > 4:
+                yield Miniature(
+                    manuscript_id=int(fields[0]),
+                    miniature_id=int(fields[1]),
+                    file_name=fields[2],
+                    start_year=eval(fields[3]),
+                    end_year=eval(fields[4]),
+                    tags=fields[5:] if len(fields) > 5 else [],
+                )
---
+++ settings.py
@@ -0,0 +1,3 @@
+MINIATURE_URL = 'http://manuscriptminiatures.com/media/manuscriptminiatures.com/original/{}'
+MINIATURES_DIR = './data/miniatures/'
+MINIATURES_METADATA = './data/miniatures_metadata.csv'
