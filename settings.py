7063bca2137297d1dc39c9e98bd1b64b01728d81
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-10-08 04:26:47 -0300

    Settings and dir for objects data

--- settings.py
+++ settings.py
@@ -4,3 +4,7 @@ from pathlib import Path
 PICTURE_URL = 'http://manuscriptminiatures.com/media/manuscriptminiatures.com/original/{}'
 PICTURES_DIR = Path('./data/pictures/')
 METADATA_CSV = Path('./data/miniatures_metadata.csv')
+OBJECTS_DIR = Path('./data/objects/')
+
+# objects tagger
+OBJECT_PREVIEW = Path('./last_object_preview.png')
