8a62e456531f83802313335e5a90197a9d29ce69
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-10-08 04:47:51 -0300

    Code to store tagged objects info for each miniature

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import attr
@@ -14,10 +15,15 @@ class Miniature:
     start_year = attr.ib()
     end_year = attr.ib()
     tags = attr.ib()
+    objects = attr.ib(init=False)
 
     @property
     def picture_path(self):
         return settings.PICTURES_DIR / self.file_name
+
+    @property
+    def objects_path(self):
+        return settings.OBJECTS_DIR / '{}.json'.format(self.miniature_id)
 
     def download_picture(self):
         """
@@ -33,6 +39,26 @@ class Miniature:
             if self.picture_path.exists():
                 self.picture_path.unlink()
             raise
+
+    def load_objects(self):
+        """
+        Load tagged objects from the json file.
+        """
+        if self.objects_path.exists():
+            with self.objects_path.open() as objects_file:
+                self.objects = [TaggedObject.deserialize(raw_object_data)
+                                for raw_object_data in json.load(objects_file)]
+        else:
+            self.objects = []
+
+    def save_objects(self):
+        """
+        Save tagged objects into the json file.
+        """
+        with self.objects_path.open('w') as objects_file:
+            raw_data = [tagged_object.serialize()
+                        for tagged_object in self.objects]
+            json.dump(raw_data, objects_file)
 
     def __str__(self):
         return '<Miniature {} ({})>'.format(self.miniature_id, self.file_name)
@@ -59,3 +85,23 @@ class Miniature:
                     end_year=eval(fields[4]),
                     tags=tags,
                 )
+
+
+@attr.s
+class TaggedObject:
+    name = attr.ib()
+    position = attr.ib()
+
+    def serialize(self):
+        """
+        Serialize tagged object data.
+        """
+        return self.name, self.position
+
+    @classmethod
+    def deserialize(cls, raw_data):
+        """
+        Reconstruct tagged object from serialized data.
+        """
+        name, position = raw_data
+        return TaggedObject(name=name, position=position)
